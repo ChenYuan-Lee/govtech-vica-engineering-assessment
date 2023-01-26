a99c49ffdce4c95ebd7907060a399467b63150f0
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-01-26 14:50:46 +0800

    Add borrow book endpoint

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 
-from routes import user, book
+from routes import user, book, borrow_return
 
 app = FastAPI()
 
 
 app.include_router(user.router)
 app.include_router(book.router)
+app.include_router(borrow_return.router)
 
 
 @app.get("/")
---
+++ routes/borrow_return.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+
+from fastapi import APIRouter, HTTPException
+
+from database import get_book, get_user, update_book
+from models.book import BorrowingAvailabilityStatus
+
+router = APIRouter(prefix="/borrow_return", tags=["borrow_return"])
+
+
+@router.put("/borrow")
+async def borrow_book(book_id: str, user_id: str):
+    user = await get_user(user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail=f"User {user_id} doesn't exist")
+
+    book = await get_book(book_id)
+    if not book:
+        raise HTTPException(status_code=404, detail=f"Book {book_id} doesn't exist")
+
+    if book['borrowing_availability_status'] == BorrowingAvailabilityStatus.UNAVAILABLE.name:
+        raise HTTPException(status_code=400, detail=f"Book {book_id} is unavailable for borrowing")
+
+    # add user into the 'borrowed_by' entry
+    book['borrowed_by'][user_id] = datetime.utcnow().date()
+
+    remaining_copies = book['total_copies'] - len(book['borrowed_by'])
+    if remaining_copies > 0:
+        availability_status = BorrowingAvailabilityStatus.AVAILABLE.name
+    else:
+        availability_status = BorrowingAvailabilityStatus.UNAVAILABLE.name
+
+    book_updated = await update_book(
+        id=book_id,
+        data={
+            'borrowed_by': book['borrowed_by'],
+            'borrowing_availability_status': availability_status
+        }
+    )
+    if book_updated:
+        return f'User {user_id} successfully borrowed a copy of book {book_id}'
+
+
+@router.put("/return")
+async def return_book(book_id: str):
+    pass
