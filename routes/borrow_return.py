7768bf5660f39d510ca8ea387dc6d1df91486d41
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-01-26 23:19:43 +0800

    create retrieve_user and retrieve_book functions to reduce code repeats

--- routes/borrow_return.py
+++ routes/borrow_return.py
@@ -8,15 +8,26 @@ from models.book import BorrowingAvailabilityStatus
 router = APIRouter(prefix="/borrow_return", tags=["borrow_return"])
 
 
-@router.put("/borrow")
-async def borrow_book(book_id: str, user_id: str):
+async def retrieve_user(user_id: str):
     user = await get_user(user_id)
     if not user:
         raise HTTPException(status_code=404, detail=f"User {user_id} doesn't exist")
 
+    return user
+
+
+async def retrieve_book(book_id: str):
     book = await get_book(book_id)
     if not book:
         raise HTTPException(status_code=404, detail=f"Book {book_id} doesn't exist")
+
+    return book
+
+
+@router.put("/borrow")
+async def borrow_book(book_id: str, user_id: str):
+    await retrieve_user(user_id)
+    book = await retrieve_book(book_id)
 
     if book['borrowing_availability_status'] == BorrowingAvailabilityStatus.UNAVAILABLE.name:
         raise HTTPException(status_code=403, detail=f"Book {book_id} is unavailable for borrowing")
@@ -24,7 +35,7 @@ async def borrow_book(book_id: str, user_id: str):
     if user_id in book['borrowed_by']:
         raise HTTPException(status_code=403, detail=f'User {user_id} already borrowed a copy of book {book_id}')
 
-    # add user into the 'borrowed_by' entry
+    # add user to the book's "borrowed_by" dict
     book['borrowed_by'][user_id] = datetime.utcnow().date()
 
     remaining_copies = book['total_copies'] - len(book['borrowed_by'])
@@ -46,18 +57,13 @@ async def borrow_book(book_id: str, user_id: str):
 
 @router.put("/return")
 async def return_book(book_id: str, user_id: str):
-    user = await get_user(user_id)
-    if not user:
-        raise HTTPException(status_code=404, detail=f"User {user_id} doesn't exist")
-
-    book = await get_book(book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail=f"Book {book_id} doesn't exist")
+    await retrieve_user(user_id)
+    book = await retrieve_book(book_id)
 
     if user_id not in book['borrowed_by']:
         raise HTTPException(status_code=403, detail=f"User {user_id} did not borrow book {book_id}")
 
-    # remove user
+    # remove user from the book's "borrowed_by" dict
     book['borrowed_by'].pop(user_id)
 
     book_updated = await update_book(
