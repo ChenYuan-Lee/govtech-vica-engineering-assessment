902b72c83fa554a8df0304ce564661ff63f1eafd
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-01-26 12:12:42 +0800

    Create user update data model

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 from datetime import date
 from enum import Enum
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -14,6 +15,21 @@ class UserSchema(BaseModel):
     name: str
     role: UserRole
     date_joined: date
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "John Doe",
+                "role": "MEMBER",
+                "date_joined": "2023-01-01",
+            }
+        }
+
+
+class UpdateUserModel(BaseModel):
+    name: Optional[str]
+    role: Optional[UserRole]
+    date_joined: Optional[date]
 
     class Config:
         schema_extra = {
