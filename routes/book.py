40cc848e815793de72710958cbfeeff0f4e4d0d2
ChenYuan-Lee <leechenyuan.95@gmail.com>
2023-01-26 12:56:40 +0800

    Create endpoint to delete books

--- routes/book.py
+++ routes/book.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
 
-from database import add_book
+from database import add_book, delete_book
 from models.book import BookSchema
 
 router = APIRouter(prefix="/book", tags=["book"])
@@ -12,3 +12,12 @@ async def add_book_data(book: BookSchema):
     book = jsonable_encoder(book)
     new_book = await add_book(book)
     return new_book
+
+
+@router.delete("/{id}", response_description="Book data deleted from the database")
+async def delete_book_data(id: str):
+    book_deleted = await delete_book(id)
+    if book_deleted:
+        return f"Book {id} deleted successfully"
+    else:
+        raise HTTPException(status_code=404, detail=f"Book {id} does not exist")
